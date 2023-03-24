0ea9046a63fc5bd044a29bde5aa11ece17a18fd2
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-03-24 14:41:31 +0700

    Add Home Page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import express from "express";
-import {router} from "./router/home.js";
+import session from "express-session";
 import {config} from "dotenv";
+import {router} from "./router/home.js";
 
 config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'padv',
+    cookie: {maxAge: 60000000}
+}));
 
 app.use('/', router);
 
--- src/router/home.js
+++ src/router/home.js
@@ -1,6 +1,14 @@
 import express from "express"
+import Console from "console";
+
 export const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('home.ejs')
+})
+
+router.get('/write', (req, res) => {
+    const text = req.query.textarea;
+    Console.log(text);
+    res.render('write.ejs')
 })
